0e10f412c296d1a7f4aeff7013e2ca2f1589bec2
gunktp14 <arrliver@gmail.com>
2024-02-05 14:46:13 +0700

    edit email confirmation form

--- src/utils/emailVerification.ts
+++ src/utils/emailVerification.ts
@@ -136,7 +136,7 @@ export const FORM_VERIFY_EMAIL = (token: string) => {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1966fb" style="border-radius: 6px;">
-                            <a href="http://localhost:3000/verification/email/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
+                            <a href="http://localhost:3000/auth/email/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
                           </td>
                         </tr>
                       </table>
