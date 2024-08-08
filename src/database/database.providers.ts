7fb793be41a42d52122bfc7c0c404a45dfbcacd7
sekkarin <644259021@webmail.npru.ac.th>
2024-08-08 19:03:40 +0700

    test: remove mongodb-memory-server

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,6 +1,5 @@
 import * as mongoose from 'mongoose';
 import { ConfigService } from '@nestjs/config';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const configService = new ConfigService();
 
@@ -8,20 +7,12 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> => {
-      if (process.env.NODE_ENV === 'test') {
-        const mongod = await MongoMemoryServer.create();
-        const uri = mongod.getUri();
-        return mongoose.connect(uri, {
+      return mongoose.connect(
+        configService.get<string>('DATABASE_URL') || process.env.DATABASE_URL,
+        {
           dbName: 'warering-project',
-        });
-      } else {
-        return mongoose.connect(
-          configService.get<string>('DATABASE_URL') || process.env.DATABASE_URL,
-          {
-            dbName: 'warering-project',
-          },
-        );
-      }
+        },
+      );
     },
   },
 ];
